f814ad4bcad8b020569a660415b4676fa147e1a9
Max McKinney <maxgames101@gmail.com>
2014-02-25 12:48:01 -0600

    Applied source sans pro font to the whole page.

--- app/js/app.js
+++ app/js/app.js
@@ -9,6 +9,8 @@ controller('todoCtrl', ['$scope', function ($scope) {
 		$scope.todos.push({'name': $scope.newTodo,'done': false})
 		$scope.newTodo = ''
 	}
+	
+	// TODO: (1) Fix bug where clearing the completed tasks keeps an "empty" item in the array.
 	$scope.clearCompleted = function(){
 		// Set the list of todos to the everything that isn't "done" yet.
 		// Basically this makes it seem like we are removing the items that are compelted.
